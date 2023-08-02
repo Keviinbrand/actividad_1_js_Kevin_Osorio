208e00f01cf48033f549b6b3655b163c51f6e17e
Keviinbrand <kevinbrand2004@gmail.com>
2023-08-01 19:29:06 -0500

    Corrección de código

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ let age = 18;
 const yearOfBirth = 2004;
 let city = 'Medellín';
 
-console.log('Hola mi nombre es ' +  name + lastName,' nací en el ' + yearOfBirth + ', tengo ' +  age + ' años vivo en ' + city +'.' )
+console.log('Hola mi nombre es ' +  name + ' '+lastName+ ' nací en el ' + yearOfBirth + ', tengo ' +  age + ' años vivo en ' + city +'.' )
 //Utilicé '+' para concatenar corractamente.
