97e68ce6d34ea8e508cc95d3c31a263cb3189518
Keviinbrand <kevinbrand2004@gmail.com>
2023-08-01 19:27:07 -0500

    Act 1 JS v2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const name = 'Kevin';
+const lastName = 'Osorio'
 let age = 18;
 const yearOfBirth = 2004;
 let city = 'Medellín';
 
-console.log('Hola mi nombre es ' +  name + ' nací en el ' + yearOfBirth + ', tengo ' +  age + ' años vivo en ' + city +'.' )
+console.log('Hola mi nombre es ' +  name + lastName,' nací en el ' + yearOfBirth + ', tengo ' +  age + ' años vivo en ' + city +'.' )
 //Utilicé '+' para concatenar corractamente.
